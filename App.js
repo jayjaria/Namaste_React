edc41bfbf16b82297ea0fb80f7cfefa1523d8757
jayjaria <jaijaria032001@gmail.com>
2024-04-01 16:35:19 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement(
+    "div", {id:"parent"},[
+        React.createElement("div",{id:"child1"},[
+            React.createElement("h1",{},"I'm a h1 tag"),
+            React.createElement("h1",{}, "I'm a h2 tag")
+        ]),
+        React.createElement("div",{id:"child2"},[
+            React.createElement("h1",{},"I'm a h1 tag"),
+            React.createElement("h1",{}, "I'm a h2 tag")
+        ])
+    ]
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
